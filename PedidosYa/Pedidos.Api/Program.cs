bcd3f42a95c65f1ac5d5e41cd9d07abdb8452d1b
FabiolaLainez <167152904+FabiolaLainez@users.noreply.github.com>
2026-09-10 20:50:54 -0600

    Creacion de endpoints y registro de httpClient en program.cs

--- PedidosYa/Pedidos.Api/Program.cs
+++ PedidosYa/Pedidos.Api/Program.cs
@@ -6,6 +6,12 @@ builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
+builder.Services.AddHttpClient("Notificaciones", client =>
+{
+    var baseUrl = builder.Configuration["Notificaciones:BaseUrl"] ?? "http://localhost:5402";
+    client.BaseAddress = new Uri(baseUrl);
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
